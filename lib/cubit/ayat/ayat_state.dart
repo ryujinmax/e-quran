f465191b326e7641d3d8b79f7c0c1772e3341137
ryujinmax <santi.rra40@gmail.com>
2024-02-01 08:06:48 +0700

    first commit

---
+++ lib/cubit/ayat/ayat_state.dart
@@ -0,0 +1,10 @@
+part of 'ayat_cubit.dart';
+
+abstract class AyatState extends Equatable {
+  const AyatState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AyatInitial extends AyatState {}
