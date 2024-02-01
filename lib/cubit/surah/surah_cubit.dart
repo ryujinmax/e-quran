9be2b7d7b1e88f14e0e76e7f5e0f749ded9c7b81
ryujinmax <santi.rra40@gmail.com>
2024-02-01 08:21:32 +0700

    Cubit fix

--- lib/cubit/surah/surah_cubit.dart
+++ lib/cubit/surah/surah_cubit.dart
@@ -1,22 +1,27 @@
 import 'package:bloc/bloc.dart';
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_ahlul_quran_app/data/model/surat_model.dart';
 import 'package:flutter_ahlul_quran_app/data/service_api.dart';
-import 'package:http/http.dart';
+
 
 part 'surah_state.dart';
+
 
 class SurahCubit extends Cubit<SurahState> {
   SurahCubit(this.apiService) : super(SurahInitial());
 
+
   final ApiService apiService;
+
 
   void getAllSurah() async {
     emit(SurahLoading());
     final result = await apiService.getAllSurah();
     result.fold(
-        (l) => SurahError(message: l), (r) => SurahLoaded(listSurah: r));
+      (l) => emit(SurahError(message: l)),
+      (r) => emit(SurahLoaded(listSurah: r)),
+    );
   }
 }
+
+
